8f64506e5776663d024ba6874d21e09aaf0fd9d9
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-12-02 15:49:32 +0100

    Fonctionalité recherche : Species

--- app/src/main/java/mathieu/r/View/Adaptater/ListSpeciesAdaptater.java
+++ app/src/main/java/mathieu/r/View/Adaptater/ListSpeciesAdaptater.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +16,9 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import mathieu.r.Model.Film;
 import mathieu.r.Model.Species;
 import mathieu.r.R;
 import mathieu.r.View.Activity.DetailObjectActivity;
@@ -25,11 +28,14 @@ import static android.support.constraint.Constraints.TAG;
 public class ListSpeciesAdaptater extends RecyclerView.Adapter<ListSpeciesAdaptater.ViewHolder> {
 
     private ArrayList<Species> dataset;                                                                //List d'object Species
+    private ArrayList<Species> datasetFull;
     private Context context;
 
     public ListSpeciesAdaptater(Context context) {
         this.context = context;
         dataset = new ArrayList<>();
+        datasetFull = new ArrayList<>(dataset);
+
     }
 
     @Override
@@ -77,8 +83,42 @@ public class ListSpeciesAdaptater extends RecyclerView.Adapter<ListSpeciesAdapta
 
     public void add(ArrayList<Species> listSpecies) {                                                     // Ajout d'un object dans la liste
         dataset.addAll(listSpecies);
+        datasetFull = new ArrayList<>(listSpecies);
         notifyDataSetChanged();
     }
+
+    public Filter getFilter() {
+        return filter;
+    }
+    private Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Species> filteredList = new ArrayList<>();
+
+            if(constraint == null || constraint.length() == 0) { // Rien ecris dans la barre
+                filteredList.addAll(datasetFull);
+            } else { // Quelque chose ecrit dans la barre de recherche
+                String filterPattern = constraint.toString().toLowerCase().trim(); // Créé une nouvel string sans espace, en minuscule
+
+                for(Species item : datasetFull) { // on parcours notre list
+                    if (item.getName().toLowerCase().contains(filterPattern)) { // Si l'item courant == taper dans la bare de recherche
+                        filteredList.add(item);
+                    }
+                }
+
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            dataset.clear();
+            dataset.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
